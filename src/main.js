d618637e1ea7242b4e558a431b878bb5f4c1d824
gunwoongPark <olympiodoros@naver.com>
2021-02-23 13:44:58 +0900

    deploy2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import "./plugins/vue-masonry";
 
+import VueFbCustomerChat from "vue-fb-customer-chat";
+
 Vue.config.productionTip = false;
+
+Vue.use(VueFbCustomerChat, {
+  page_id: 181759373588767, //  change 'null' to your Facebook Page ID,
+  theme_color: "#333333", // theme color in HEX
+  locale: "ko_KR", // default 'en_US'
+});
 
 new Vue({
   vuetify,
